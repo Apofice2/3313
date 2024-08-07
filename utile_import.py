2eefeafc339ea5844a7441f1e23105876ba22e1f
matth <matthieu.alauze@gmail.com>
2024-08-07 23:08:27 +0200

    nouvelle version
    Signed-off-by: matth <matthieu.alauze@gmail.com>

--- utile_import.py
+++ utile_import.py
@@ -13,7 +13,7 @@ import os
 # chemind'accès de tout les fichiers
 
 # images simples
-IMG_BLOCK_METAL = "zortch_unused2//bigdoor3.png"
-IMG_BOUTON_MENU = "industrial//standard-button-off.png"
-IMG_BOUTON_DEFAUT = "modern/button-off.png"
+IMG_BLOCK_METAL = "asset//zortch_unused2//bigdoor3.png"
+IMG_BOUTON_MENU = "asset//industrial//standard-button-off.png"
+IMG_BOUTON_DEFAUT = "asset//modern/button-off.png"
 
